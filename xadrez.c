5fc9ad17ed83dfe5f16280625fabc9a021f2a3d8
Ivan Bahia <bahiaivan@gmail.com>
2025-05-19 22:16:48 +0000

    tabuleiro batalha naval, nivel novato, posicionando dois navios

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,50 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+int main()
+{
+	char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+	
+	int tabuleiro[10][10]; // Declarando a matriz tabuleiro
+	
+	for(int i = 0; i < 10; i++) // Iniciando o tabuleiro[i][j] com valores i * 10 + j
+	{
+		for(int j = 0; j < 10; j++)
+		{
+			tabuleiro[i][j] = (i * 10 + j) * 0; // multiplica todos os valores por zero
+			
+		}
+	}
+	//printf("Tabuleiro 10 x 10...\n"); // Imprimindo o tabuleiro de 1 a 10 linhas e de A a J colunas
+		printf("\n");
+		printf("   "); // Imprime um espaço entre os títulos das colunas
+		for(int j = 0; j < 10; j++)
+		{
+			printf(" %c ", 'A' + j); // imprime o cabeçalho da coluna de A até J
+			
+		}
+		//Posição do navio na horizontal linha 7 (indice 6) e colunas C, D, e E (indices 2, 3, e 4)
+		tabuleiro[6][2] = 3;
+		tabuleiro[6][3] = 3;
+		tabuleiro[6][4] = 3;
+		//posição do navio na vertical coluna 7 (indice 6) e linhas 2, 3, e 4 (indices 1, 2, e 3)
+		tabuleiro[1][6] = 3;
+		tabuleiro[2][6] = 3;
+		tabuleiro[3][6] = 3;
+		
+			printf("\n"); // quebra de linha entre o cabeçalho da coluna e o tabuleiro
+			
+		for(int i = 0; i < 10; i++)
+		{
+			printf("%2d", i + 1); // Imprime a ordem das linhas com caracteres de no mínimo 2 digitos.
+			for(int j = 0; j < 10; j++)
+			{
+				printf("%3d", tabuleiro[i][j]); //imprimindo o tabuleiro
+				
+			}
+			printf("\n");
+		}
+			
+	
+	return 0;
 }
